c3bddad87e1cdd90cf55e3d855d9451b4a4bbb34
VictorFG <victor.fernandez.gayan@gmail.com>
2021-02-21 19:41:20 +0100

    .

--- js/loadMap.js
+++ js/loadMap.js
@@ -24,9 +24,8 @@ register(proj4);
 const projection = getProjection('EPSG:25831');
 projection.setExtent([257904,4484796,535907,4751795]);
 
-const extent = [257904,4484796,535907,4751795];
+const extent = [257904,4284796,515907,4751795];
 const view = new View({
-    zoom: 4,
     projection: projection,
     center: [396905,4618292],
     resolutions: [275,100,50,25,10,5,2,1,0.5,],
@@ -36,7 +35,7 @@ const view = new View({
 
 var source = new VectorSource();
 
-const layers = new LayerGroup({
+const baseLayers = new LayerGroup({
     layers:[
         new TileLayer({
             extent: [257904,4484796,535907,4751795],
@@ -46,7 +45,8 @@ const layers = new LayerGroup({
                     'LAYERS': 'topo'
                 }
             }),
-            title:'topo'
+            title:'topo',
+            visible:false
         }),
         new TileLayer({
             extent: [257904,4484796,535907,4751795],
@@ -56,10 +56,10 @@ const layers = new LayerGroup({
                     'LAYERS': 'orto25c'
                 }
             }),
-            title:'orto',
-            visible:false
-        }),
-        new VectorLayer({
+            title:'orto'
+        })
+        //dibuixar layer
+        /*new VectorLayer({
             source: source,
             style: new Style({
                 fill: new Fill({
@@ -77,8 +77,84 @@ const layers = new LayerGroup({
                 }),
             }),
             visible:true
-        })
+        })*/
     ]
+})
+
+const comarquesLayers = new LayerGroup({
+  layers:[
+      new TileLayer({
+        extent: [257904,4484796,535907,4751795],
+        source: new TileWMS({
+            url: 'http://geoserveis.icc.cat/icgc_bm5m/wms/service?',
+            params: {
+                'LAYERS': '20_COMARCA_PC'
+            }
+        }),
+        title:'municipis1',
+        visible:true
+      }),
+      new TileLayer({
+        extent: [257904,4484796,535907,4751795],
+        source: new TileWMS({
+            url: 'http://geoserveis.icc.cat/icgc_bm5m/wms/service?',
+            params: {
+                'LAYERS': '50_NOMCAP_TX'
+            }
+        }),
+        title:'municipis2',
+        visible:true
+      }),
+      new TileLayer({
+        extent: [257904,4484796,535907,4751795],
+        source: new TileWMS({
+            url: 'https://geoserveis.icgc.cat/icgc_bm5m/wms/service?',
+            params: {
+                'LAYERS': '40_TOPO_TX,70_NOMCOMARCA_TX'
+            }
+        }),
+        title:'municipis3',
+        visible:true
+      })
+  ]
+})
+
+const municipisLayers = new LayerGroup({
+  layers:[
+      new TileLayer({
+        extent: [257904,4484796,535907,4751795],
+        source: new TileWMS({
+            url: 'http://geoserveis.icc.cat/icgc_bm5m/wms/service?',
+            params: {
+                'LAYERS': '02_TIPUSLINIA_LN'
+            }
+        }),
+        title:'municipis1',
+        visible:false
+      }),
+      new TileLayer({
+        extent: [257904,4484796,535907,4751795],
+        source: new TileWMS({
+            url: 'http://geoserveis.icc.cat/icgc_bm5m/wms/service?',
+            params: {
+                'LAYERS': '50_NOMCAP_TX'
+            }
+        }),
+        title:'municipis2',
+        visible:false
+      }),
+      new TileLayer({
+        extent: [257904,4484796,535907,4751795],
+        source: new TileWMS({
+            url: 'https://geoserveis.icgc.cat/icgc_bm5m/wms/service?',
+            params: {
+                'LAYERS': '40_TOPO_TX,70_NOMCOMARCA_TX'
+            }
+        }),
+        title:'municipis3',
+        visible:false
+      })
+  ]
 })
 
 var dragAndDropInteraction = new DragAndDrop({
@@ -88,7 +164,7 @@ var dragAndDropInteraction = new DragAndDrop({
 const map = new Map({
     interactions: defaultInteractions().extend([dragAndDropInteraction]),
     target: 'map',
-    layers: layers,
+    layers: [baseLayers,comarquesLayers,municipisLayers],
     view: view,
     controls : defaultControls({
         attribution : false,
@@ -96,20 +172,89 @@ const map = new Map({
     }),
 });
 
-// Switcher for Layers
-const baseLayerElements = document.querySelectorAll('#inputsLayers > input[type=radio]');
+//BASE LAYERS
+
+// Switcher for Layers //
+const baseLayerElements = document.querySelectorAll('#inputsLayers > div > input[type=radio]');
 
 for (let baseLayerElements of baseLayerElements){
     baseLayerElements.addEventListener('change', function(){
         let baseLayerElementValue = this.value;
-        layers.getLayers().forEach(function(element,index,array){
+        baseLayers.getLayers().forEach(function(element,index,array){
             let baseLayerTitle = element.get('title');
             element.setVisible(baseLayerTitle === baseLayerElementValue);
         })
     })
 }
 
+// Opacity for Layers //
+document.getElementById("topoOpacity").addEventListener("change", topoOpacity);
+document.getElementById("ortoOpacity").addEventListener("change", ortoOpacity);
+
+function topoOpacity(e) {
+  var opacityInput = $('#topoOpacity');
+  baseLayers.getLayers().forEach(function(lyr,index,array) {
+    if (lyr.get('title') == 'topo'){
+      lyr.setOpacity(parseFloat(opacityInput[0].value));
+      opacityInput.val(String(lyr.getOpacity()));
+    }
+  });
+}
+
+function ortoOpacity(e) {
+  var opacityInput = $('#ortoOpacity');
+  baseLayers.getLayers().forEach(function(lyr,index,array) {
+    if (lyr.get('title') == 'orto'){
+      lyr.setOpacity(parseFloat(opacityInput[0].value));
+      opacityInput.val(String(lyr.getOpacity()));
+    }
+  });
+}
+
+//SECONDARY LAYERS
+
+// Switcher for Layers //
+document.getElementById("comarques").addEventListener("change", changeVisibility);
+
+function changeVisibility() {
+  comarquesLayers.getLayers().forEach(function(lyr,index,array) {
+      var is_visible = lyr.get('visible');
+      lyr.setVisible(!is_visible);
+  });
+}
+
+document.getElementById("municipis").addEventListener("change", changeVisibility2);
+
+function changeVisibility2() {
+  municipisLayers.getLayers().forEach(function(lyr,index,array) {
+      var is_visible = lyr.get('visible');
+      lyr.setVisible(!is_visible);
+  });
+}
+
+// Opacity for Layers //
+document.getElementById("comarquesOpacity").addEventListener("change", comarquesOpacity);
+document.getElementById("municipisOpacity").addEventListener("change", municipisOpacity);
+
+function comarquesOpacity(e) {
+  var opacityInput = $('#comarquesOpacity');
+  comarquesLayers.getLayers().forEach(function(lyr,index,array) {
+    lyr.setOpacity(parseFloat(opacityInput[0].value));
+    opacityInput.val(String(lyr.getOpacity()));
+  });
+}
+
+function municipisOpacity(e) {
+  var opacityInput = $('#municipisOpacity');
+  municipisLayers.getLayers().forEach(function(lyr,index,array) {
+    lyr.setOpacity(parseFloat(opacityInput[0].value));
+    opacityInput.val(String(lyr.getOpacity()));
+  });
+}
+
 // Draw Features
+
+/*
 function clearMap() {
     source.clear();
 }
@@ -136,8 +281,11 @@ typeSelect.onchange = function () {
 
 document.getElementById("drawingTypeDelete").addEventListener("click", clearMap);
 
+*/
 
 // Drag-and-Drop
+
+/*
 dragAndDropInteraction.on('addfeatures', function (event) {
     var vectorSource = new VectorSource({
       features: event.features,
@@ -149,3 +297,5 @@ dragAndDropInteraction.on('addfeatures', function (event) {
     );
     map.getView().fit(vectorSource.getExtent());
 });
+
+*/
